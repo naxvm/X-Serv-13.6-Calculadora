ece4eba34dc387b54c6b38b7f4ddc479c8da20f1
Ignacio Condes Menchen <icondes@gamma28.aulas.gsyc.urjc.es>
2017-02-07 10:28:15 +0100

    Resolución intermedia

--- calculadora.py
+++ calculadora.py
@@ -1,33 +1,20 @@
 #! /usr/bin/python3.5
+
 import sys
 
-arguments = sys.argv
+if len(sys.argv) != 4:
+	sys.exit("Usage: calc.py operacion operando1 operando2")
 
-operation = arguments[1]
-firstArg = int(arguments[2])
-secondArg = int(arguments[3])
+_, operation, firstArg, secondArg = sys.argv
+
+firstArg = float(firstArg)
+secondArg = float(secondArg)
 
 
-if operation == 'suma':
-	charOp = '+'
-	result = firstArg + secondArg
 
-elif operation == 'resta':
-	charOp = '-'
-	result = firstArg - secondArg
 
-elif operation == 'multiplicacion':
-	charOp = '*'
-	result = firstArg * secondArg
 
-elif operation == 'division':
-	charOp = '/'
-	try:
-		result = firstArg / secondArg
-	except ZeroDivisionError:
-		sys.exit("Error: división entre cero no permitida")
 
-else:
-	sys.exit("Operación no reconocida. suma, resta, multiplicacion, division")
-
-print(firstArg, charOp, secondArg, '=', result)
+# distinto orden de argumentos
+# excepcion de argumentos, operacion no entendida (en el else), division entre cero 	
+# menor numero posible de lineas dentro de un try
