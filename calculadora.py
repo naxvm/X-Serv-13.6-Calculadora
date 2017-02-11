c981bad576355e4895fad2b727f2d2c1fe23dd12
Nacho Condés <i.condes@alumnos.urjc.es>
2017-02-11 11:01:17 +0100

    Ejercicio terminado

--- calculadora.py
+++ calculadora.py
@@ -3,18 +3,33 @@
 import sys
 
 if len(sys.argv) != 4:
-	sys.exit("Usage: calc.py operacion operando1 operando2")
+    sys.exit("Semántica: calc.py operacion operando1 operando2")
 
 _, operation, firstArg, secondArg = sys.argv
 
-firstArg = float(firstArg)
-secondArg = float(secondArg)
+try:
 
+    firstArg = float(firstArg)
+    secondArg = float(secondArg)
+except ValueError:
+    sys.exit("Semántica: calc.py operacion operando1 (int/float) operando2 (int/float)")
 
+if operation == 'suma':
+    result = firstArg + secondArg
 
+elif operation == 'resta':
+    result = firstArg - secondArg
 
+elif operation == 'multiplicacion':
+    result = firstArg * secondArg
 
+elif operation == 'division':
+    try:
+        result = firstArg / secondArg
+    except ZeroDivisionError:
+        sys.exit('ERROR: división entre cero detectada')
 
-# distinto orden de argumentos
-# excepcion de argumentos, operacion no entendida (en el else), division entre cero 	
-# menor numero posible de lineas dentro de un try
+else:
+    sys.exit('Operación no comprendida. Prueba suma, resta, multiplicacion, division')
+
+print(result)
